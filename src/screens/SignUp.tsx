b0ca66d281f38d93362f9382d8a1438868514825
Renato Ferraz <renato.ferraz@qodeless.io>
2025-04-20 19:44:50 -0300

    Mover o texto de erro do Input que está na tela SignUp para dentro do componente Input

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,88 +1,102 @@
-import { Center, Heading, Image, ScrollView, Text, VStack } from "@gluestack-ui/themed";
+import {
+  Center,
+  Heading,
+  Image,
+  ScrollView,
+  Text,
+  VStack,
+} from '@gluestack-ui/themed'
 
-import { useNavigation } from "@react-navigation/native";
-import { AuthNavigatorRoutesProps } from "@routes/auth.routes";
+import { useNavigation } from '@react-navigation/native'
 
-import BackgroundImg from '@assets/background.png';
-import Logo from '@assets/logo.svg';
+import BackgroundImg from '@assets/background.png'
+import Logo from '@assets/logo.svg'
 
-import { Button } from "@components/Button";
-import { Input } from "@components/Input";
-import { Controller, useForm } from "react-hook-form";
+import { Button } from '@components/Button'
+import { Input } from '@components/Input'
+import { Controller, useForm } from 'react-hook-form'
 
 type FormDataProps = {
-  name: string;
-  email: string;
-  password: string;
-  passwordConfirm: string;
+  name: string
+  email: string
+  password: string
+  password_confirm: string
 }
 
 export function SignUp() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormDataProps>()
 
-  const { control, handleSubmit, formState: {errors}} = useForm<FormDataProps>();
+  const navigation = useNavigation()
 
-  const { navigate } = useNavigation<AuthNavigatorRoutesProps>();
-  
-  function handleNavigateToSignIn() {
-    navigate('signIn')
+  function handleGoBack() {
+    navigation.goBack()
   }
 
-  function handleSignUp({ name, email, password, passwordConfirm }: FormDataProps ) {
-    console.log({name, email, password, passwordConfirm})
+  function handleSignUp({
+    name,
+    email,
+    password,
+    password_confirm,
+  }: FormDataProps) {
+    console.log({ name, email, password, password_confirm })
   }
 
   return (
-    <ScrollView 
+    <ScrollView
       contentContainerStyle={{ flexGrow: 1 }}
       showsVerticalScrollIndicator={false}
     >
       <VStack flex={1}>
         <Image
+          w="$full"
+          h={624}
           source={BackgroundImg}
           defaultSource={BackgroundImg}
           alt="Pessoas treinando"
           position="absolute"
-          w="$full"
-          h={624}
         />
+
         <VStack flex={1} px="$10" pb="$16">
           <Center my="$24">
             <Logo />
+
             <Text color="$gray100" fontSize="$sm">
-              Treine sua mente e o seu corpo
+              Treine sua mente e seu corpo
             </Text>
           </Center>
-          <Center gap="$2" flex={1}>
-            <Heading color="$gray100" textAlign="center">
-              Crie sua conta
-            </Heading>
+
+          <Center flex={1} gap="$2">
+            <Heading color="$gray100">Crie sua conta</Heading>
 
             <Controller
               control={control}
               name="name"
-              rules={{ 
-                required: 'Informe seu nome' 
+              rules={{
+                required: 'Informe o nome',
               }}
               render={({ field: { onChange, value } }) => (
-                <Input 
+                <Input
                   placeholder="Nome"
-                  autoCapitalize="none"
                   onChangeText={onChange}
                   value={value}
+                  errorMessage={errors.name?.message}
                 />
               )}
             />
-            {errors.name?.message && (<Text color="$gray100">{errors.name.message}</Text>)}
 
             <Controller
               control={control}
               name="email"
               rules={{
-                required: 'Informe o email.',
+                required: 'Informe o e-mail',
                 pattern: {
-                  value:/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                  message: 'E-mail inválido'
-                }
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: 'E-mail inválido',
+                },
               }}
               render={({ field: { onChange, value } }) => (
                 <Input
@@ -91,10 +105,10 @@ export function SignUp() {
                   autoCapitalize="none"
                   onChangeText={onChange}
                   value={value}
+                  errorMessage={errors.email?.message}
                 />
               )}
             />
-             {errors.email?.message && (<Text color="white">{errors.email?.message}</Text>)}
 
             <Controller
               control={control}
@@ -111,10 +125,10 @@ export function SignUp() {
 
             <Controller
               control={control}
-              name="passwordConfirm"
+              name="password_confirm"
               render={({ field: { onChange, value } }) => (
                 <Input
-                  placeholder="Confirmar senha"
+                  placeholder="Confirme a Senha"
                   secureTextEntry
                   onChangeText={onChange}
                   value={value}
@@ -123,12 +137,21 @@ export function SignUp() {
                 />
               )}
             />
-            
-            <Button title="Criar e acessar" onPress={handleSubmit(handleSignUp)}/>
+
+            <Button
+              title="Criar e acessar"
+              onPress={handleSubmit(handleSignUp)}
+            />
           </Center>
-          <Button title="Voltar para login" variant="outline" mt={"$12"} onPress={handleNavigateToSignIn}/>
+
+          <Button
+            title="Voltar para o login"
+            variant="outline"
+            mt="$12"
+            onPress={handleGoBack}
+          />
         </VStack>
       </VStack>
     </ScrollView>
-  );
+  )
 }
