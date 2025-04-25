c3cd4aa11c95c16d5872bbae987e5a664221b881
Renato Ferraz <renato.ferraz@qodeless.io>
2025-04-24 23:35:48 -0300

    Criar atualização de estados no contexto de auth na tela de login

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -9,11 +9,40 @@ import Logo from '@assets/logo.svg';
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useAuth } from "@hooks/useAuth";
+import { Controller, useForm } from "react-hook-form";
+import * as yup from 'yup';
+
+type FormDataProps = {
+  email: string
+  password: string
+}
+
+const signInSchema = yup.object({
+  email: yup
+    .string()
+    .required('Informe o e-mail'),
+  password: yup
+    .string()
+    .required('Informe a senha')
+})
+
 export function SignIn() {
+  const { signIn } = useAuth();
+
   const { navigate } = useNavigation<AuthNavigatorRoutesProps>();
+
+  const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
+    resolver: yupResolver(signInSchema)
+  })  
   
   function handleNewAccount() {
     navigate('signUp')
+  }
+
+  function handleSignIn({ email, password }: FormDataProps) {
+    signIn(email, password)
   }
 
   return (
@@ -41,9 +70,36 @@ export function SignIn() {
             <Heading color="$gray100" textAlign="center">
               Acesse sua conta
             </Heading>
-            <Input placeholder="E-mail" keyboardType="email-address" autoCapitalize="none"/>
-            <Input placeholder="Senha" secureTextEntry/>
-            <Button title="Acessar"/>
+            <Controller
+              control={control}
+              name="email"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="E-mail"
+                  keyboardType="email-address"
+                  autoCapitalize="none"
+                  onChangeText={onChange}
+                  value={value}
+                  onSubmitEditing={handleSubmit(handleNewAccount)}
+                  errorMessage={errors.email?.message}
+                /> 
+              )}
+            />
+            <Controller
+              control={control}
+              name="password"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="Senha"
+                  secureTextEntry
+                  onChangeText={onChange}
+                  value={value}
+                  onSubmitEditing={handleSubmit(handleNewAccount)}
+                  errorMessage={errors.password?.message}
+                /> 
+              )}
+            />
+            <Button title="Acessar" onPress={handleSubmit(handleSignIn)}/>
           </Center>
           <Center flex={1} justifyContent="flex-end">
             <Text color="$gray100" fontSize="$sm" mb="$2" fontFamily="$body">
