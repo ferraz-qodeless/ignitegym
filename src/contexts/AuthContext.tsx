994c2b784c1e0d852531c303b6bd0d7da5cee6a0
Renato Ferraz <renato.ferraz@qodeless.io>
2025-04-27 17:55:59 -0300

    Persistir token (JWT) do usuário logado, Anexar token nas requisições

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,6 +1,7 @@
 import { createContext, ReactNode, useEffect, useState } from "react";
 
 import { UserDTO } from "@dtos/UserDTO";
+import { storageAuthTokenSave } from "@storage/user/storage-auth-token";
 import { storageUserGet, storageUserRemove, storageUserSave } from "@storage/user/storage-user";
 
 import { api } from '@services/api';
@@ -22,18 +23,31 @@ export function AuthContextProvider({ children }: AuthContextProviderProps)  {
   const [user, setUser] = useState<UserDTO>({} as UserDTO)
   const [isLoadingUserStorageData, setIsLoadingStorageData] = useState(true);
 
+  async function storageUserAndToken(userData: UserDTO, token: string) {
+    try {
+      setIsLoadingStorageData(true);
+
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`
+
+      await storageUserSave(userData)
+      await storageAuthTokenSave(token)
+      setUser(userData);
+    } catch (error) {
+      throw error
+    } finally {
+      setIsLoadingStorageData(false);
+    }
+  }
   async function signIn(email: string, password: string) {
     try {
       const { data } = await api.post('/sessions', { email, password });
-      if(data.user) {
-        setUser(data.user);
-        storageUserSave(data.user)
+      if(data.user && data.token) {
+        storageUserAndToken(data.user, data.token);
       }
     } catch (error) {
       throw error
     }
   }
-
   async function signOut() {
     try {
       setIsLoadingStorageData(true);
