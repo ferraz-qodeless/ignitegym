b036482a6c06407f0e076af916bcc5d5407b8892
Renato Ferraz <renato.ferraz@qodeless.io>
2025-04-30 22:31:21 -0300

    Recuperar imagem do usuário no banco

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,6 +1,7 @@
 import defaultUserPhotoImg from '@assets/userPhotoDefault.png';
 import { HStack, Icon, Text, VStack } from "@gluestack-ui/themed";
 import { useAuth } from "@hooks/useAuth";
+import { api } from '@services/api';
 import { LogOut } from "lucide-react-native";
 import { TouchableOpacity } from 'react-native';
 import { UserPhoto } from "./UserPhoto";
@@ -12,7 +13,11 @@ export function HomeHeader() {
   return (
     <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center" gap='$4'>
       <UserPhoto 
-        source={user.avatar ? { uri: user.avatar } : defaultUserPhotoImg}
+        source={
+          user.avatar 
+          ? { uri: `${api.defaults.baseURL}/avatar/${user.avatar}` } 
+          : defaultUserPhotoImg
+        }
         w='$16' 
         h='$16' 
         alt='Imagem do usuário'
